a51829035c2ab9887679f6922311ffdafa3c20fb
Tomas Lingotti <tomaslingotti@gmail.com>
2022-02-06 04:00:47 -0300

    wip

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-7 w-full flex flex-wrap justify-around gap-5 md:gap-8 lg:gap-5">
     <ul>
-      <li v-for="movie in Movies" :key="movie.imdbID">
+      <li v-for="movie in Movies" :key="movie.imdbID" class="item">
         <div>
           <p>{{ movie.Title }}</p>
           <img :src="movie.Poster" :alt="movie.imdbID"/>
@@ -28,9 +28,36 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css?family=Roboto');
+
+
+@media (min-width: 1200px) {
+  #app {
+    padding-left: 80px;
+    padding-right: 80px;
+  }
+}
+
 ul li {
   list-style: none;
 }
+
+html { font-size: 22px; }
+body { font-family: 'Roboto', sans-serif; }
+
+a {
+  color: #0094FF;
+  text-decoration: none;
+}
+
+a:hover { color: #0074C6; }
+
+.item {
+  padding: 16px 24px;
+  border-radius: 3px;
+  background: #F5F5F5;
+}
+
 </style>
 
 
